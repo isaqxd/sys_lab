8f28918ec86ab69e94af890616f18e5f192dded1
Isaq <isaque3356@icloud.com>
2025-11-12 12:47:38 -0300

    atualização final hotfix-bd

--- src/app/routes/slot-reserva.js
+++ src/app/routes/slot-reserva.js
@@ -17,10 +17,10 @@ router.post('/save', (req, res) => {
         return db.query(query, [values], (err, result) => {
             if (err) {
                 console.error(err);
-                return res.status(500).json({ error: 'Erro ao cadastrar status reserva' });
+                return res.status(500).json({ error: 'Erro ao cadastrar Slot Reserva' });
             }
             return res.status(201).json({
-                message: `${result.affectedRows} status reserva cadastrados com sucesso`
+                message: `${result.affectedRows} Slot Reserva cadastrados com sucesso`
             });
         });
     }
@@ -28,15 +28,15 @@ router.post('/save', (req, res) => {
     const { fk_reserva_id, fk_horario_disponivel_id} = req.body;
     const query = 'INSERT INTO slot_reserva (fk_reserva_id, fk_horario_disponivel) VALUES (?, ?)';
     db.query(query, [fk_reserva_id, fk_horario_disponivel_id], (err, result) => {
-        if (err) return res.status(500).json({ error: 'Erro ao cadastrar status reserva' });
-        res.status(201).json({ message: 'Status reserva cadastrado com sucesso', recursoId: result.insertId });
+        if (err) return res.status(500).json({ error: 'Erro ao cadastrar Slot Reserva' });
+        res.status(201).json({ message: 'Slot Reserva cadastrado com sucesso', recursoId: result.insertId });
     });
 });
 
 router.get('/findAll', (req, res) => {
     const query = 'SELECT * FROM slot_reserva';
     db.query(query, (err, results) => {
-        if (err) return res.status(500).json({ error: 'Erro ao buscar Status reserva' });
+        if (err) return res.status(500).json({ error: 'Erro ao buscar slot_reservas'});
         res.status(200).json(results);
     });
 });
@@ -45,8 +45,8 @@ router.get('/findById/:id', (req, res) => {
     const { id } = req.params;
     const query = 'SELECT * FROM slot_reserva WHERE id_slot_reserva = ?';
     db.query(query, [id], (err, results) => {
-        if (err) return res.status(500).json({ error: 'Erro ao buscar Status reserva' });
-        if (results.length === 0) return res.status(404).json({ message: 'Status reserva não encontrado' });
+        if (err) return res.status(500).json({ error: 'Erro ao buscar Slot Reserva' });
+        if (results.length === 0) return res.status(404).json({ message: 'Slot Reserva não encontrado' });
         res.status(200).json(results[0]);
     });
 });
@@ -56,8 +56,8 @@ router.put('/updateById/:id', (req, res) => {
     const { descricao } = req.body;
     const query = 'UPDATE slot_reserva SET fk_reserva_id = ?, fk_horario_id = ? WHERE id_slot_reserva = ?';
     db.query(query, [descricao, id], (err, result) => {
-        if (err) return res.status(500).json({ error: 'Erro ao atualizar Status reserva' });
-        res.status(200).json({ message: 'Status reserva atualizado com sucesso' });
+        if (err) return res.status(500).json({ error: 'Erro ao atualizar Slot Reserva' });
+        res.status(200).json({ message: 'Slot Reserva atualizado com sucesso' });
     });
 });
 
@@ -72,8 +72,8 @@ router.patch('/updatePartial/:id', (req, res) => {
     const query = `UPDATE slot_reserva SET ${fields.join(', ')} WHERE id_slot_reserva = ?`;
     values.push(id);
     db.query(query, values, (err, result) => {
-        if (err) return res.status(500).json({ error: 'Erro ao atualizar Status reserva' });
-        res.status(200).json({ message: 'Status reserva atualizado com sucesso' });
+        if (err) return res.status(500).json({ error: 'Erro ao atualizar Slot Reserva' });
+        res.status(200).json({ message: 'Slot Reserva atualizado com sucesso' });
     });
 });
 
@@ -81,8 +81,8 @@ router.delete('/deleteById/:id', (req, res) => {
     const { id } = req.params;
     const query = 'DELETE FROM slot_reserva WHERE id_slot_reserva = ?';
     db.query(query, [id], (err, result) => {
-        if (err) return res.status(500).json({ error: 'Erro ao deletar Status reserva' });
-        res.status(200).json({ message: 'Status reserva deletado com sucesso' });
+        if (err) return res.status(500).json({ error: 'Erro ao deletar Slot Reserva' });
+        res.status(200).json({ message: 'Slot Reserva deletado com sucesso' });
     });
 });
 
