f09188cc910d2fb12f30274b9d93aaabae7a7ded
Isaq <isaque3356@icloud.com>
2025-11-21 20:28:33 -0300

    alteracao do com o novo banco de dados, nova pasta para controllers, novos comandos adaptando o novo banco de dados, atualização de rotas

--- src/app/routes/api.js
+++ src/app/routes/api.js
@@ -1,23 +1,13 @@
+// app/routes/api.js
 const express = require('express');
 const router = express.Router();
 
-const usuarioRoutes = require('./usuario');
-router.use('/usuario', usuarioRoutes);
-
-const horarioDisponivel = require('./horario-disponivel');
-router.use ('/horarioDisponivel', horarioDisponivel);
-
-const salaRoutes = require('./sala');
-router.use('/sala', salaRoutes);
-
-const recursoRoutes = require('./recurso');
-router.use('/recurso', recursoRoutes);
-
-const slotRoutes = require('./slot-reserva');
-router.use('/slotReserva', slotRoutes);
-
-const reservaRoutes = require('./reserva');
-router.use('/reserva', reservaRoutes);
-
+router.use('/auditoria', require('../controllers/auditoria'));
+router.use('/horario', require('../controllers/horario'));
+router.use('/recurso', require('../controllers/recurso'));
+router.use('/reserva', require('../controllers/reserva'));
+router.use('/sala-recurso', require('../controllers/sala-recurso'));
+router.use('/sala', require('../controllers/sala'));
+router.use('/usuario', require('../controllers/usuario'));
 
 module.exports = router;
--- src/server.js
+++ src/server.js
@@ -1,22 +1,17 @@
 const express = require('express');
-const app = express();
-const port = 3000;
-
-const db = require('./app/db');
 const path = require('path');
+const app = express();
 
-app.use(express.static('public'));
-const apiRoutes = require('./app/routes/api');
+require('./app/db');
 
-app.use(express.static('public'));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'))
-})
-
+const apiRoutes = require('./app/routes/api');
 app.use('/api', apiRoutes);
 
-app.listen(port, () => {
-  console.log(`Servidor funcionando`)
-})
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+module.exports = app;
