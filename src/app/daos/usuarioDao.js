47356d744e4babf473de0fb9ba10c69385f8fc0c
SunshasDev <luiseduardomaia04@gmail.com>
2025-11-23 22:51:19 -0300

    Criação do Login e portal ADMIN e portal PROFESSOR

--- src/app/daos/usuarioDao.js
+++ src/app/daos/usuarioDao.js
@@ -1,5 +1,15 @@
 const db = require('../db');
 
+// Busca um usuário pelo email para verificar a senha depois
+function buscarPorEmail(email, callback) {
+    const sql = 'SELECT * FROM usuario WHERE email = ? AND status_usuario = 1';
+    db.query(sql, [email], callback);
+}
+
+module.exports = {
+    buscarPorEmail
+};
+
 function salvarUsuario(usuario, callback) {
     const { nome, email, senha, tipo, status_usuario } = usuario;
     const querySave = 'INSERT INTO usuario (nome,email,senha,tipo,status_usuario) VALUES (?, ?, ?, ?, ?)';
@@ -65,5 +75,6 @@ module.exports = {
     buscarPorId,
     atualizarUsuario,
     atualizarParcial,
-    desativarSala
+    desativarSala,
+    buscarPorEmail
 };
--- src/app/routes/api.js
+++ src/app/routes/api.js
@@ -9,5 +9,6 @@ router.use('/reserva', require('../controllers/reservaController'));
 router.use('/sala-recurso', require('../controllers/salaRecursoController'));
 router.use('/sala', require('../controllers/salaController'));
 router.use('/usuario', require('../controllers/usuarioController'));
+router.use('/auth', require('../controllers/authController'));
 
 module.exports = router;
